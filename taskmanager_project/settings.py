cb65129b88d0a17a10842aba13a06804226d3460
Deepanshu24cg <deepanshu24cg@gmail.com>
2026-07-24 06:55:25 +0530

    update

--- taskmanager_project/settings.py
+++ taskmanager_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vd$8u-#uh6ssvfw!ci_%h*ggk4tklgzv!4aku-!%komdcvz^lv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
