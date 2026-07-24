5ec64eb385aeb20c170c42e6c814b2f5292e695b
Deepanshu24cg <deepanshu24cg@gmail.com>
2026-07-24 06:40:42 +0530

    update

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,7 @@
-from django.shortcuuts import render
+from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Task
+def home(request):
+    return HttpResponse("Welcome to the Task Manager!")
+
 
