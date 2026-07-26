b0f375151368145ea1de02bebb24268c615195e8
Deepanshu24cg <deepanshu24cg@gmail.com>
2026-07-26 12:39:49 +0530

    update

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
 
 def home(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()  # Saves the new task to the database!
+            return redirect('home')
+    else:
+        form = TaskForm()
+
     tasks = Task.objects.all()
-    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+    return render(request, 'tasks/task_list.html', {'tasks': tasks, 'form': form})
